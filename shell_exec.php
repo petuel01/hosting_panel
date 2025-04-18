0f64074a2d9a7c5eec1fafb3a647701d69a7ccd5
petzeus <baifempetuel0.2@gmail.com>
2025-04-18 12:50:28 +0100

    v 2.1.6

--- shell_exec.php
+++ shell_exec.php
@@ -1,4 +1,11 @@
 <?php
-$output = shell_exec('ls -l');
-echo "<pre>$output</pre>";
+$output1 = shell_exec('ls -l');
+$output2 = shell_exec('whoami');
+$output3 = shell_exec('uptime');
+
+echo "<pre>";
+echo "List of files:\n$output1\n";
+echo "Current user:\n$output2\n";
+echo "System uptime:\n$output3\n";
+echo "</pre>";
 ?>
